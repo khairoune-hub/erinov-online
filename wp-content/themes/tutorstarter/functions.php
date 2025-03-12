fc1d8427e640484637c697e3515279d3d74bf559
Khairoune Moussa <74827513+khairoune-hub@users.noreply.github.com>
2025-03-12 21:11:49 +0100

    Update functions.php

--- wp-content/themes/tutorstarter/functions.php
+++ wp-content/themes/tutorstarter/functions.php
@@ -30,33 +30,3 @@ endif;
 if ( class_exists( 'Tutor_Starter\\Init' ) ) :
 	Tutor_Starter\Init::register_services();
 endif;
-
-// Add phone number field to the main user profile section
-add_action( 'user_new_form', 'add_phone_number_field' );
-add_action( 'show_user_profile', 'add_phone_number_field' );
-add_action( 'edit_user_profile', 'add_phone_number_field' );
-
-function add_phone_number_field( $user ) {
-	$phone_number = is_object( $user ) ? get_user_meta( $user->ID, 'phone_number', true ) : '';
-	?>
-	<h3><?php esc_html_e( 'User Information', 'tutor' ); ?></h3>
-	<table class="form-table">
-		<tr>
-			<th><label for="phone_number"><?php esc_html_e( 'Phone Number', 'tutor' ); ?></label></th>
-			<td>
-				<input type="text" name="phone_number" id="phone_number" value="<?php echo esc_attr( $phone_number ); ?>" class="regular-text" />
-			</td>
-		</tr>
-	</table>
-	<?php
-}
-
-// Save the phone number field
-add_action( 'personal_options_update', 'save_phone_number_field' );
-add_action( 'edit_user_profile_update', 'save_phone_number_field' );
-
-function save_phone_number_field( $user_id ) {
-	if ( isset( $_POST['phone_number'] ) ) {
-		update_user_meta( $user_id, 'phone_number', sanitize_text_field( $_POST['phone_number'] ) );
-	}
-}
